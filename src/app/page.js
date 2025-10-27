b10e191c513320e3c7a53a7a3df3a13a4c14d4e1
mcd-94 <marco.email.it@gmail.com>
2025-10-27 18:33:35 +0100

    a

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import Card from "@/components/Card/Card";
 import ImageToBase64 from "@/components/ImageToBase64/ImageToBase64";
 export default function Home() {
   const [specialties, setSpecialties] = useState([]);
+  const [healthInsurances, setHealthInsurances] = useState([]);
 
   useEffect(() => {
     async function fetchSpecialties() {
@@ -19,8 +20,19 @@ export default function Home() {
         console.error(error);
       }
     }
+    async function fetchHealthInsurances() {
+      try {
+        const res = await fetch("/healthinsurances");
+        if (!res.ok) throw new Error("Error al obtener las obras sociales");
 
+        const data = await res.json();
+        setHealthInsurances(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
     fetchSpecialties();
+    fetchHealthInsurances();
   }, []);
 
   return (
@@ -33,9 +45,11 @@ export default function Home() {
           bookingButton="true"
         />
         <section className="p-6">
-          <header className="mb-4">
-            <h2 className="text-2xl font-semibold">Servicios Médicos</h2>
-            <p className="text-gray-600">
+          <header className="mb-4 flex flex-col gap-1">
+            <h2 className="text-3xl font-semibold text-[#3478a2]">
+              Servicios Médicos
+            </h2>
+            <p className="text-xl text-gray-600">
               Contamos con una amplia variedad de estudios, respaldados por un
               equipo de profesionales con años de experiencia.
             </p>
@@ -53,6 +67,27 @@ export default function Home() {
               ))
             ) : (
               <p>Cargando especialidades...</p>
+            )}
+          </div>
+        </section>
+        <section className="p-6">
+          <header className="mb-4 flex flex-col gap-1">
+            <h2 className="text-3xl font-semibold text-[#3478a2]">
+              Obras Sociales
+            </h2>
+            <p className="text-xl text-gray-600">
+              En MEDICALIDW tenemos convenios con diversas obras sociales para
+              facilitar tu acceso a nuestros servicios.
+            </p>
+          </header>
+
+          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {healthInsurances.length > 0 ? (
+              healthInsurances.map((e) => (
+                <Card key={e._id} image={e.image} title={e.name} />
+              ))
+            ) : (
+              <p>Cargando obras sociales...</p>
             )}
           </div>
         </section>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 const Card = ({ title, description, image }) => {
   return (
-    <div className="border rounded-md">
-      <h3>{title}</h3>
-      {image && (
-        <img
-          src={image}
-          alt={title}
-          className="w-32 h-32 object-cover rounded mt-2"
-        />
-      )}
+    <div className="border rounded-md flex flex-col justify-between overflow-hidden">
+      <div>
+        {image && (
+          <img src={image} alt={title} className="w-full h-auto object-cover" />
+        )}
+      </div>
+      <div className="border-0 border-t-1 text-center">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
